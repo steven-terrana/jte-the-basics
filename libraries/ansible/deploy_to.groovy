d6edd549f800ebf42487475416ba97bf62af720a
steven-terrana <steventerrana@gmail.com>
2019-11-05 09:51:32 -0500

    Update deploy_to.groovy

--- libraries/ansible/deploy_to.groovy
+++ libraries/ansible/deploy_to.groovy
@@ -1,6 +1,8 @@
 void call(app_env){
-    println "performing a deployment through ansible.."
-    app_env.ip_addresses.each{ ip ->
-        println "deploying to ${ip}"
+    stage("Deploy To: ${app_env.long_name}"){
+        println "performing a deployment through ansible.."
+        app_env.ip_addresses.each{ ip ->
+            println "deploying to ${ip}"
+        }
     }
 }
