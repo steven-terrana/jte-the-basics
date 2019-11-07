4fcf22fcb4c11abe8b954b72548ae4d74d3f5766
steven-terrana <steventerrana@gmail.com>
2019-11-07 13:03:42 -0500

    Update splunk_step_watcher.groovy

--- libraries/splunk/splunk_step_watcher.groovy
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -3,7 +3,7 @@ void before(context){
   println "Splunk: running before the ${context.library} library's ${context.step} step" 
 }
 
-@AfterStep
+@AfterStep({ context.step.equals("static_code_analysis") })
 void after(context){
   println "Splunk: running after the ${context.library} library's ${context.step} step" 
 }
