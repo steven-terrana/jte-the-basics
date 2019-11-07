76ee2a84c9659cbc897bcee82b8f9ca551139c0f
steven-terrana <steventerrana@gmail.com>
2019-11-06 20:02:56 -0500

    Update splunk_before_step.groovy

--- libraries/splunk/splunk_before_step.groovy
+++ libraries/splunk/splunk_before_step.groovy
@@ -1,9 +1,9 @@
 @BeforeStep
 void before(context){
-  println "Splunk: running before the ${context.library}'s ${context.step} step" 
+  println "Splunk: running before the ${context.library} library's ${context.step} step" 
 }
 
 @AfterStep
 void after(context){
-  println "Splunk: running after the ${context.library}'s ${context.step} step" 
+  println "Splunk: running after the ${context.library} library's ${context.step} step" 
 }
