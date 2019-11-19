318126dd15d8a0d914ef101aeb9c5d5bc8a1ac91
steven-terrana <steventerrana@gmail.com>
2019-11-19 10:27:15 -0800

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,4 +1,17 @@
 libraries{
-  merge = true 
-  sonarqube
+    merge = true
+    sonarqube
+    ansible
+    splunk
+}
+
+
+application_environments{
+    dev{
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod{
+        long_name = "Production"
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
 }
