f7080bcaf15bc479bdcc3771292cadd77d84d83f
steven-terrana <steventerrana@gmail.com>
2020-04-29 17:31:08 -0400

    Update splunk_step_watcher.groovy

--- libraries/splunk/splunk_step_watcher.groovy
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -1,8 +1,6 @@
-@BeforeStep({ 
-  println(currentBuild.getDisplayName())
-  currentBuild.getDisplayName().equals("single-pipeline")
-})
+@BeforeStep
 void before(context){
+  println currentBuild.getDisplayName()
   println "Splunk: running before the ${context.library} library's ${context.step} step" 
 }
 
