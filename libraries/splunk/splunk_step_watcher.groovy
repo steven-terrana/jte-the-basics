1f4bbd61cef0b84c3355208f3e07cb0be29b34f6
steven-terrana <steventerrana@gmail.com>
2020-04-29 21:27:01 -0400

    Update splunk_step_watcher.groovy

--- libraries/splunk/splunk_step_watcher.groovy
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -1,7 +1,13 @@
-@BeforeStep({ currentBuild.getDisplayName().equals("#30") })
+@BeforeStep
 void before(context){
   println currentBuild.getDisplayName()
   println "Splunk: running before the ${context.library} library's ${context.step} step" 
+}
+
+@BeforeStep
+void before2(context){
+  println currentBuild.getDisplayName()
+  println "Splunk: running before2 the ${context.library} library's ${context.step} step" 
 }
 
 @AfterStep({ config.afterSteps ?  (context.step in config.afterSteps) : true })
