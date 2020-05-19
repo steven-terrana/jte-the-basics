249603cc8c323ff0ac81eb893fc821744ea35794
steven-terrana <steventerrana@gmail.com>
2020-05-19 08:39:59 -0400

    Update splunk_step_watcher.groovy

--- libraries/splunk/splunk_step_watcher.groovy
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -6,5 +6,4 @@ void before(context){
 @AfterStep({ config.afterSteps ? (context.step in config.afterSteps) : true })
 void after(context){
   println "Splunk: running after the ${context.library} library's ${context.step} step" 
-  println "Splunk: current status: ${currentBuild.result.toString()}"
 }
