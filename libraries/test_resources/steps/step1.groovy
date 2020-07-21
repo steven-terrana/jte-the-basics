38cf8373560c9d0d9b4fe77e2baa7d9f2b93ead2
steven-terrana <steventerrana@gmail.com>
2020-07-21 14:56:30 -0400

    Update step1.groovy

--- libraries/test_resources/steps/step1.groovy
+++ libraries/test_resources/steps/step1.groovy
@@ -1,6 +1,8 @@
 void call(){
   println "step 1 from test_resources library"
-  
-  println "fetching somethingElse.json" 
   println resource("somethingElse.json")
+  node{
+    sh resource("someresource.sh")
+  }
+  println resource("nestedDirectory/someOtherFile.txt")
 }
